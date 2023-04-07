a31a69088c6939909205635889a75fff29346a7e
tedyistiawan <tedypnd07@gmail.com>
2023-04-07 10:41:18 +0700

    exercise 8

--- easD_1_191_D/easD_1_191_D.cpp
+++ easD_1_191_D/easD_1_191_D.cpp
@@ -1,6 +1,6 @@
 // dengan adanya alogaritma maka dapat meningkatkan kinerja dan produktivitas dalam dunia IT untuk menyelesaikan masalah.
 // struktur data linear dan non - linear tersebut
-// ukuran masukkan, waktu eksekusi, pemilihan alogaritma yang tepat, kompleksitas alogaritma, bahasa pemograman, kualitas implementasi
+// ukuran masukkan, waktu eksekusi, pemilihan alogaritma yang tepat, kompleksitas alogaritma, bahasa pemograman, kualitas implementasinya
 // insertion sort, karena Algoritma ini efisien pada data kecil, waktu yang dibutuhkan untuk melakukan perbandingan dan pergeseran data relatif sedikit.
 // Quadratic yaitu bubble sort, insertion sort, selection sort. log linear yaitu merge sort,quick sort, heap sort.
 #include <iostream>
