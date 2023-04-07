8eab26873b2d1960997026190d0198274431c880
tedyistiawan <tedypnd07@gmail.com>
2023-04-07 10:11:18 +0700

    exercise1

---
+++ easD_1_191_D/easD_1_191_D.cpp
@@ -0,0 +1,16 @@
+// dengan adanya alogaritma maka dapat meningkatkan kinerja dan produktivitas dalam dunia IT untuk menyelesaikan masalah.
+// struktur data linear dan non - linear
+// ukuran masukkan, waktu eksekusi, pemilihan alogaritma yang tepat, kompleksitas alogaritma, bahasa pemograman, kualitas implementasi
+// insertion sort, karena Algoritma ini efisien pada data kecil, waktu yang dibutuhkan untuk melakukan perbandingan dan pergeseran data relatif sedikit.
+// Quadratic yaitu bubble sort, insertion sort, selection sort. log linear yaitu merge sort,quick sort, heap sort.
+#include <iostream>
+#include <string>
+using namespace std;
+
+// di Inputkan Procedure
+void input(int arr[], int size) {
+    cout << "Enter " << size << " integer elements of the array:" << endl;
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+}
